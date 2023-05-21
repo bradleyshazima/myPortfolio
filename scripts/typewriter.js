2f3b61884d357bd230d42f1f24b97985f6501c16
Bradley Shilton <bradyshilton@gmail.com>
2023-05-21 11:45:15 +0300

    Added the projects section. Added a favicon and changed the cursor.

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -1,43 +1,3 @@
-// const words = ['Full Stack Web Developer', 'Certified Graphic Designer', 'Self Taught UI/UX Designer'];
-// let wordIndex = 0;
-// let letterIndex = 0;
-// let direction = 1;
-// const textElement = document.getElementById('hero-text');
-
-// function typeEffect() {
-//   const currentWord = words[wordIndex];
-//   if (direction === 1) {
-//     textElement.textContent = currentWord.slice(0, letterIndex);
-//     letterIndex++;
-//     if (letterIndex > currentWord.length) {
-//       direction = -1;
-//       setTimeout(typeEffect, 2000); // Delay before deleting
-//     } else {
-//       setTimeout(typeEffect, 100); // Delay between typing each letter
-//     }
-//   } else {
-//     if (letterIndex === 0 && currentWord === '') {
-//       direction = 1;
-//       wordIndex = (wordIndex + 1) % words.length;
-//       setTimeout(typeEffect, 0); // No delay when hero-text is blank
-//     } else if (letterIndex === 0) {
-//       textElement.textContent = currentWord.slice(0, letterIndex);
-//       direction = 1;
-//       wordIndex = (wordIndex + 1) % words.length;
-//       setTimeout(typeEffect, 4000); // Delay before next word typing
-//     } else {
-//       textElement.textContent = currentWord.slice(0, letterIndex);
-//       letterIndex--;
-//       setTimeout(typeEffect, 100); // Delay between deleting each letter
-//     }
-//   }
-// }
-
-// typeEffect();
-
-
-
-
 const words = ['Full Stack Web Developer', 'Certified Graphic Designer', 'Self Taught UI/UX Designer'];
 let wordIndex = 0;
 let letterIndex = 0;
@@ -61,7 +21,7 @@ function typeEffect() {
       textElement.textContent = currentWord.slice(0, letterIndex);
       direction = 1;
       wordIndex = (wordIndex + 1) % words.length;
-      setTimeout(typeEffect, currentWord === '' ? 0 : 4000); // No delay when hero-text is empty
+      setTimeout(typeEffect, currentWord === '' ? 0 : 500); // No delay when hero-text is empty
     } else {
       textElement.textContent = currentWord.slice(0, letterIndex);
       letterIndex--;
